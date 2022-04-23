2da266ecc8f65773d3e37d6d81bc6f768d9569ad
Temirlan006 <ahunzanovtemirlan@gmail.com>
2022-04-23 19:51:53 +0600

    lesson6

---
+++ lesson3/main.js
@@ -0,0 +1,32 @@
+// function sayHello() {
+//
+// }
+//
+// sayHello();
+//
+//
+// function getFullName(user, age,) {
+//     if(age < 18){
+//         console.log("CLOSE!")
+//     }else{
+//         console.log(user.first_name, user.last_name)
+//     }
+// }
+//
+const user = {
+    first_name: "John",
+    last_name: "Barbaro"
+}
+//
+// const user2 = {
+//     first_name: "Vito",
+//     last_name: "Scalette"
+// }
+//
+// getFullName(user, 15);
+// getFullName(user2, 19);
+
+const anonym = function (user) {
+    console.log(user)
+}
+anonym();
